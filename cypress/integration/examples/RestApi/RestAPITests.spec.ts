95235b275ad500e465c86a4a7cf9aedf3d28e173
chakraborty.rupam <chakraborty.rupam@outlook.com>
2020-04-07 19:50:55 +0530

    new Rest API tests

--- cypress/integration/examples/RestApi/RestAPITests.spec.ts
+++ cypress/integration/examples/RestApi/RestAPITests.spec.ts
@@ -2,9 +2,9 @@
 /// <reference types="cypress" />
 
 
-describe('API Suite', () =>{    
+describe('REST API Suite', () =>{    
       
-context('Rest API on https://pokeapi.co/api/v2/pokemon', () =>{
+context('Rest API on pokeapi.co/api/v2/pokemon', () =>{
 
 beforeEach(()=>{
 cy.request('GET','/api/v2/pokemon').as('GetPoke')
@@ -48,6 +48,52 @@ cy.log('inside API Suite')
                 cy.log("Url for white-2 : "+ JSON.stringify(url2));
             })
         })        
+    })
+
+})
+
+// Tests for Jsonplaceholder API
+context('Rest API on jsonplaceholder.typicode.com', () => 
+{
+    before('set base url', () => 
+    {
+        Cypress.config('baseUrl', 'https://jsonplaceholder.typicode.com');
+        cy.log('base url set to : https://jsonplaceholder.typicode.com');
+    })
+
+    it('POST on jsonplaceholder', () => 
+    {
+       // read file 
+       cy.readFile('cypress/integration/examples/RestApi/RequestJSONs/PlaceholderPostRequest.json').then(jsonObj =>
+       {
+           // parse json to modify values
+           cy.log('json file contents before modification: '+ JSON.stringify(jsonObj));            
+           jsonObj.title = 'Rupam';
+           jsonObj.body = 'Body of Json File';
+           jsonObj.userId = 404;
+           cy.log('json file contents after modification: '+ JSON.stringify(jsonObj)); 
+
+           // invoke Post and evaluate response
+           cy.request('POST', '/posts', JSON.stringify(jsonObj)).then((response) =>
+           {
+               cy.log('response status :' + (<any>response).status);
+               expect((<any>response).status).to.eq(201);
+               //cy.log('response body: ' + JSON.stringify((<any>response)));
+               cy.log('response statusText : ' + (<any>response).statusText);
+           }) 
+       })
+    })
+
+    it('GET on jsonplaceholder', () => 
+    {
+        // invoke get and evaluate response
+        cy.request('GET','/posts/1').then((response) =>
+        {
+            cy.log('response status :' + (<any>response).status);
+            expect((<any>response).status).to.eq(200);
+            expect((<any>response).body.id).to.eq(1);
+            cy.log('response statusText : ' + (<any>response).statusText);
+        }) 
     })
 
 })

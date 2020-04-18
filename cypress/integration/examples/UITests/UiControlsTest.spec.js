df2455538c9d875eb10263df9f3a4877a230882c
chakraborty.rupam <chakraborty.rupam@outlook.com>
2020-04-18 17:10:06 +0530

    Adding new tests for CB, DDL, FloatingDDL

--- cypress/integration/examples/UITests/UiControlsTest.spec.js
+++ cypress/integration/examples/UITests/UiControlsTest.spec.js
@@ -18,10 +18,9 @@ cy.get('@TextBox').then(($txtbox) => {
 
 // Click on Hide to verify input control is not visible on page
  cy.get('#hide-textbox').click({force: true})
- //
- cy.wrap(cy.get('@TextBox').attribute('style'))
  cy.get('@TextBox').attribute('style').should('contain','display: none;')
  cy.get('@TextBox').invoke('attr','style').should('contain','display: none;')
+ cy.get('@TextBox').should('have.attr','style','display: none;')
  cy.get('@TextBox').then($text => {
     expect($text).to.have.attr('style','display: none;')
  })
@@ -30,8 +29,40 @@ cy.get('@TextBox').then(($txtbox) => {
  cy.get('#show-textbox').click({force: true})
  cy.get('@TextBox').then($text => {
     expect($text).to.have.attr('style','display: block;')
- })
+ }) 
+})
 
+it('Test CheckBox', () => {
+    cy.get('input[type=\'checkbox\']').as('checkboxElements')
+    cy.log('Traverse through all the options of the CB and check them')
+    cy.get('@checkboxElements').each(($cb, index, $list) =>{
+        cy.wrap($cb).check().should('be.checked')        
+    })
+
+    cy.log('Check an option based on the value \'option3\'')
+    cy.get('@checkboxElements').check('option3')
+
+    cy.log('Filters the list for \'option1\ and then uncheks the specified CB')
+    cy.get('@checkboxElements').filter('[value=\'option1\']').uncheck().should('not.be.checked')
+    })
+
+it('Test Select DDL', () =>{
+    // select by text
+    cy.get('#dropdown-class-example').select('Option1')
+    // select by value
+    cy.get('#dropdown-class-example').select('option3')
+})
+
+it('Test Radio Button', ()=>{
+    cy.get('#radio-btn-example input[type=\'radio\']').as('RadioButtonCollection')
+    cy.get('@RadioButtonCollection').filter('[value=\'radio1\']').check().should('be.checked')    
+    cy.get('#radio-btn-example').contains('Radio3').find('input').check().should('be.checked')
+})
+
+it('Test Folating DDL', () =>{
+    cy.get('#autocomplete').as('DDL')
+    cy.get('@DDL').type('tan').wait(1000)
+    cy.get('.ui-menu-item-wrapper').contains('Tanzania').click()
 })
 
 })

cc328c5469775f8a9486805ffbabf49631a1f21a
chakraborty.rupam <chakraborty.rupam@outlook.com>
2020-04-18 12:07:12 +0530

    refinement

--- cypress/integration/examples/UITests/UiControlsTest.spec.js
+++ cypress/integration/examples/UITests/UiControlsTest.spec.js
@@ -9,23 +9,23 @@ beforeEach(()=>{
 
 it('Test IO Text', () =>{
 // Set and Get Text
-cy.get('#displayed-text').type('Test Input')
-cy.pause(1000)
+cy.get('#displayed-text').as('TextBox')
+cy.get('@TextBox').type('Test Input').wait(1000)
 
-cy.get('#displayed-text').then(($txtbox) => { 
+cy.get('@TextBox').then(($txtbox) => { 
     expect($txtbox).to.have.value('Test Input')
 })
 
 // Click on Hide to verify input control is not visible on page
  cy.get('#hide-textbox').click({force: true})
- cy.log('style attrib value post hiding is: '+ cy.get('#displayed-text').attribute('style'))
- cy.get('#displayed-text').then($text => {
+ cy.log('style attrib value post hiding is: '+ cy.get('@TextBox').attribute('style'))
+ cy.get('@TextBox').then($text => {
     expect($text).to.have.attr('style','display: none;')
  })
 
  // Click on Show to verify control is visible
  cy.get('#show-textbox').click({force: true})
- cy.get('#displayed-text').then($text => {
+ cy.get('@TextBox').then($text => {
     expect($text).to.have.attr('style','display: block;')
  })
 

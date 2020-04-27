faefbad7000bafab3dd87700fca190f419ca4417
chakraborty.rupam <chakraborty.rupam@outlook.com>
2020-04-27 10:13:50 +0530

    Cleanups

--- cypress/integration/examples/RestApi/RestAPITests.spec.ts
+++ cypress/integration/examples/RestApi/RestAPITests.spec.ts
@@ -6,45 +6,40 @@ describe('REST API Suite', () =>{
       
 context('Rest API on pokeapi.co/api/v2/pokemon', () =>{
 
-beforeEach(()=>{
-cy.request('GET','/api/v2/pokemon').as('GetPoke')
-cy.log('inside API Suite')
-})
-
     it('Get Call Invoke',() => {
-        cy.get('@GetPoke').then((response) =>
+        cy.request('GET','/api/v2/pokemon').then(response =>
         {      
-            cy.log((<any>response).status)
-            expect((<any>response).status).to.eq(200)
-            expect((<any>response).body.results[0]).to.have.property('url')
-            cy.log((<any>response).body.results[0].name)
-            cy.log(JSON.stringify((<any>response).body))
+            cy.log(response.status.toString())
+            expect(response.status).to.eq(200)
+            expect(response.body.results[0]).to.have.property('url')
+            cy.log(response.body.results[0].name)
+            cy.log(JSON.stringify(response.body))
         })
     })
 
     it('Query Json Respnose using LINQ Filters', () => {
-        cy.get('@GetPoke').then((response) => 
+        cy.request('GET','/api/v2/pokemon').then(response => 
         {
             cy.log("Printing all Results ==>");
-            (<any>response).body.results.forEach(e => cy.log(e));
+            response.body.results.forEach(e => cy.log(e));
             cy.log("PRINTING DETAILS FOR CHARIZAD ==>");
-            (<any> response).body.results.filter(f => f.name === 'charizard').forEach(e => cy.log(e));
+            response.body.results.filter(f => f.name === 'charizard').forEach(e => cy.log(e));
             cy.log("PRINTING DETAILS FOR NAMES STARTING WITH 'b'  ==>");
-            (<any>response).body.results.filter(n => n.name.startsWith("b")).forEach(e => cy.log(e))        
+            response.body.results.filter(n => n.name.startsWith("b")).forEach(e => cy.log(e))        
         });
     })  
     
     it('Query url for name squirtle', () => 
     {         
-        cy.get('@GetPoke').then((response) => 
+        cy.request('GET','/api/v2/pokemon').then(response => 
         {
-            var url = (<any>response).body.results.filter(f => f.name === 'squirtle')[0].url;
+            var url = response.body.results.filter(f => f.name === 'squirtle')[0].url;
             cy.log("Url for Squirtel : "+ JSON.stringify(url));
             cy.request('GET',url).then((response) => 
             {
                 cy.log("Printing all Abilities Results ==>");
-                (<any>response).body.abilities.forEach(e => cy.log(e.ability));
-                var url2 = (<any> response).body.game_indices.filter(a => a.version.name === 'white-2')[0].version.url;
+                response.body.abilities.forEach(e => cy.log(e.ability));
+                var url2 = response.body.game_indices.filter(a => a.version.name === 'white-2')[0].version.url;
                 cy.log("Url for white-2 : "+ JSON.stringify(url2));
             })
         })        
@@ -73,12 +68,12 @@ context('Rest API on jsonplaceholder.typicode.com', () =>
            cy.log('json file contents after modification: '+ JSON.stringify(jsonObj)); 
 
            // invoke Post and evaluate response
-           cy.request('POST', '/posts', JSON.stringify(jsonObj)).then((response) =>
+           cy.request('POST', '/posts', JSON.stringify(jsonObj)).then(response =>
            {
-               cy.log('response status :' + (<any>response).status);
-               expect((<any>response).status).to.eq(201);
+               cy.log('response status :' + response.status);
+               expect(response.status).to.eq(201);
                //cy.log('response body: ' + JSON.stringify((<any>response)));
-               cy.log('response statusText : ' + (<any>response).statusText);
+               cy.log('response statusText : ' + response.statusText);
            }) 
        })
     })
@@ -86,12 +81,12 @@ context('Rest API on jsonplaceholder.typicode.com', () =>
     it('GET on jsonplaceholder', () => 
     {
         // invoke get and evaluate response
-        cy.request('GET','/posts/1').then((response) =>
+        cy.request('GET','/posts/1').then(response =>
         {
-            cy.log('response status :' + (<any>response).status);
-            expect((<any>response).status).to.eq(200);
-            expect((<any>response).body.id).to.eq(1);
-            cy.log('response statusText : ' + (<any>response).statusText);
+            cy.log('response status :' + response.status);
+            expect(response.status).to.eq(200);
+            expect(response.body.id).to.eq(1);
+            cy.log('response statusText : ' + response.statusText);
         }) 
     })  
 
@@ -103,7 +98,7 @@ context('Rest API on jsonplaceholder.typicode.com', () =>
             // First Create the User to be updated
             cy.request({
                 method: 'POST',
-                url: 'https://jsonplaceholder.typicode.com/posts',
+                url: '/posts',
                 headers: {
                   'Content-Type' : 'text/json'
                 },
@@ -116,7 +111,7 @@ context('Rest API on jsonplaceholder.typicode.com', () =>
                 // Update the user now from the response object.
                 cy.request({
                     method : 'PUT',
-                    url : 'https://jsonplaceholder.typicode.com/posts/1',
+                    url : '/posts/1',
                     headers : {
                         'Content-Type' : 'text/json'
                     },
